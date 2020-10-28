6e8e0bbbd6b087927d80013ef326b010cd2efd98
Gleison de Almeida Ribeiro <gleisonsubzerokz@gmail.com>
2020-10-28 12:27:09 -0300

    feat: criação component about.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,13 +8,15 @@ import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { FooterComponent } from "./components/footer/footer.component";
 import { ToggleThemeComponent } from "./components/toggle-theme/toggle-theme.component";
 import { HomeComponent } from "./pages/home/home.component";
-import { RestaurantsComponent } from './pages/restaurants/restaurants.component';
+import { RestaurantsComponent } from "./pages/restaurants/restaurants.component";
+import { AboutComponent } from "./pages/about/about.component";
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
+    AboutComponent,
     ToggleThemeComponent,
     HomeComponent,
     RestaurantsComponent,
