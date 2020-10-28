1ec4c0bc955c705a0b7ce2ad95aad575226bffd9
Gleison de Almeida Ribeiro <gleisonsubzerokz@gmail.com>
2020-10-28 16:54:36 -0300

    style: restaurant styles.

--- src/app/pages/restaurants/restaurants.component.ts
+++ src/app/pages/restaurants/restaurants.component.ts
@@ -1,15 +1,72 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { Restaurant } from "src/app/models/restaurant";
 
 @Component({
-  selector: 'mt-restaurants',
-  templateUrl: './restaurants.component.html',
-  styleUrls: ['./restaurants.component.scss']
+  selector: "mt-restaurants",
+  templateUrl: "./restaurants.component.html",
+  styleUrls: ["./restaurants.component.scss"],
 })
 export class RestaurantsComponent implements OnInit {
+  restaurants: Restaurant[] = [
+    {
+      id: "",
+      name: "Tasty Treats",
+      category: "Bakery",
+      deliveryEstimate: "40-65m",
+      rating: 4.5,
+      imagePath: "assets/img/restaurants/tasty.png",
+    },
+    {
+      id: "",
+      name: "Tasty Treats",
+      category: "Bakery",
+      deliveryEstimate: "40-65m",
+      rating: 4.5,
+      imagePath: "assets/img/restaurants/tasty.png",
+    },
+    {
+      id: "",
+      name: "Tasty Treats",
+      category: "Bakery",
+      deliveryEstimate: "40-65m",
+      rating: 4.5,
+      imagePath: "assets/img/restaurants/tasty.png",
+    },
+    {
+      id: "",
+      name: "Tasty Treats",
+      category: "Bakery",
+      deliveryEstimate: "40-65m",
+      rating: 4.5,
+      imagePath: "assets/img/restaurants/tasty.png",
+    },
+    {
+      id: "",
+      name: "Tasty Treats",
+      category: "Bakery",
+      deliveryEstimate: "40-65m",
+      rating: 4.5,
+      imagePath: "assets/img/restaurants/tasty.png",
+    },
+    {
+      id: "",
+      name: "Tasty Treats",
+      category: "Bakery",
+      deliveryEstimate: "40-65m",
+      rating: 4.5,
+      imagePath: "assets/img/restaurants/tasty.png",
+    },
+    {
+      id: "",
+      name: "Tasty Treats",
+      category: "Bakery",
+      deliveryEstimate: "40-65m",
+      rating: 4.5,
+      imagePath: "assets/img/restaurants/tasty.png",
+    },
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
