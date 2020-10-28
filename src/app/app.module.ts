7460552523d3be2be2f83dac5e905c092afab395
Gleison de Almeida Ribeiro <gleisonsubzerokz@gmail.com>
2020-10-28 10:46:49 -0300

    feat: criação componente home.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,24 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { HeaderComponent } from './components/header/header.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { FooterComponent } from './components/footer/footer.component';
-import { ToggleThemeComponent } from './components/toggle-theme/toggle-theme.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { HeaderComponent } from "./components/header/header.component";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { FooterComponent } from "./components/footer/footer.component";
+import { ToggleThemeComponent } from "./components/toggle-theme/toggle-theme.component";
+import { HomeComponent } from "./pages/home/home.component";
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    ToggleThemeComponent
+    ToggleThemeComponent,
+    HomeComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    BrowserAnimationsModule
-  ],
+  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
