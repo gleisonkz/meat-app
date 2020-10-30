e7526adea882b6a62b95d64f3e066dc494346211
Gleison de Almeida Ribeiro <gleisonsubzerokz@gmail.com>
2020-10-30 16:51:12 -0300

    refactor: componentização menu-item.

--- src/app/components/menu-item/menu-item.component.ts
+++ src/app/components/menu-item/menu-item.component.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from "@angular/core";
+import { MenuItem } from "src/app/models/menu-item";
 
 @Component({
-  selector: 'mt-menu-item',
-  templateUrl: './menu-item.component.html',
-  styleUrls: ['./menu-item.component.scss']
+  selector: "mt-menu-item",
+  templateUrl: "./menu-item.component.html",
+  styleUrls: ["./menu-item.component.scss"],
 })
 export class MenuItemComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  @Input() item: MenuItem;
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
--- src/app/components/menu/menu.component.ts
+++ src/app/components/menu/menu.component.ts
@@ -1,15 +1,48 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { MenuItem } from "../../models/menu-item";
 
 @Component({
-  selector: 'mt-menu',
-  templateUrl: './menu.component.html',
-  styleUrls: ['./menu.component.scss']
+  selector: "mt-menu",
+  templateUrl: "./menu.component.html",
+  styleUrls: ["./menu.component.scss"],
 })
 export class MenuComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  items: MenuItem[] = [
+    {
+      id: "cup-cake",
+      imagePath: "assets/img/foods/cupcake.png",
+      name: "Cup Cake",
+      description: "Cup Cake recheado de Doce de Leite",
+      price: 8.7,
+      restaurantId: "bread-bakery",
+    },
+    {
+      id: "donut",
+      imagePath: "assets/img/foods/donut.png",
+      name: "Donut",
+      description: "Coberto com chantilly",
+      price: 2.5,
+      restaurantId: "bread-bakery",
+    },
+    {
+      id: "bread",
+      imagePath: "assets/img/foods/bread.png",
+      name: "Pão Artesanal Italiano",
+      description: "Pão artesanal com queijos italianos",
+      price: 15.9,
+      restaurantId: "bread-bakery",
+    },
+    {
+      id: "burger",
+      imagePath: "assets/img/foods/burger.png",
+      name: "Classic Burger",
+      description: "O clássico. Não tem como errar.",
+      price: 18.5,
+      restaurantId: "burger-house",
+    },
+  ];
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
