dfa623fc6e22be81488f61d7416fd735dbd53de6
Gleison de Almeida <48810597+gleisonkz@users.noreply.github.com>
2020-10-31 07:11:49 -0300

    feat: menu route configuration.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,20 @@ import { RestaurantsComponent } from "./pages/restaurants/restaurants.component"
 import { HomeComponent } from "./pages/home/home.component";
 import { AboutComponent } from "./pages/about/about.component";
 import { RestaurantDetailComponent } from "./pages/restaurant-detail/restaurant-detail.component";
+import { MenuComponent } from "./components/menu/menu.component";
 
 const routes: Routes = [
   { path: "", component: HomeComponent },
   { path: "restaurants", component: RestaurantsComponent },
+  {
+    path: "restaurants/:id",
+    component: RestaurantDetailComponent,
+    children: [
+      { path: "", redirectTo: "menu", pathMatch: "full" },
+      { path: "menu", component: MenuComponent },
+    ],
+  },
   { path: "about", component: AboutComponent },
-  { path: "restaurants/:id", component: RestaurantDetailComponent },
 ];
 
 @NgModule({
