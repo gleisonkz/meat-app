bd68745d8b84e5304c99726d212167fa57f478f9
Gleison de Almeida Ribeiro <gleisonsubzerokz@gmail.com>
2020-11-17 15:13:52 -0300

    ✨ feat: search-bar effect added.

--- src/app/pages/restaurants/restaurants.component.ts
+++ src/app/pages/restaurants/restaurants.component.ts
@@ -1,3 +1,10 @@
+import {
+  trigger,
+  state,
+  style,
+  transition,
+  animate,
+} from "@angular/animations";
 import { Component, OnInit } from "@angular/core";
 import { FormGroup } from "@angular/forms";
 import { Restaurant } from "src/app/models/restaurant";
@@ -7,10 +14,30 @@ import { RestaurantsService } from "../../services/restaurants.service";
   selector: "mt-restaurants",
   templateUrl: "./restaurants.component.html",
   styleUrls: ["./restaurants.component.scss"],
+  animations: [
+    trigger("toggleSearch", [
+      state(
+        "false",
+        style({
+          opacity: 0,
+          maxHeight: 0,
+        })
+      ),
+      state(
+        "true",
+        style({
+          opacity: 1,
+          maxHeight: "initial",
+        })
+      ),
+      transition("* => *", animate(".2s 0s ease-out")),
+    ]),
+  ],
 })
 export class RestaurantsComponent implements OnInit {
   restaurants: Restaurant[];
   searchForm: FormGroup;
+  searchBarState = false;
 
   constructor(private restaurantesService: RestaurantsService) {}
 
